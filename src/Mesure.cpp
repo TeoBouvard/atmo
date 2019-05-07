d16e5ebaa0bc689fbfae9f4c3409a30684d6c8dc
teobouvard <teobouvard@gmail.com>
2019-05-07 09:54:37 +0200

    mesure + sensor class

---
+++ src/Mesure.cpp
@@ -0,0 +1,74 @@
+/*************************************************************************
+                           Mesure  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Mesure> (fichier Mesure.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Mesure.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type Mesure::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+Mesure & Mesure::operator = ( const Mesure & unMesure )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Mesure::Mesure ( const Mesure & unMesure )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <Mesure>" << endl;
+#endif
+} //----- Fin de Mesure (constructeur de copie)
+
+
+Mesure::Mesure ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Mesure>" << endl;
+#endif
+} //----- Fin de Mesure
+
+
+Mesure::~Mesure ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Mesure>" << endl;
+#endif
+} //----- Fin de ~Mesure
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
