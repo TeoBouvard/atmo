628c564a7a845b1a80d8922ad245ac3e19fbe81c
mrisbourg <mrisbourg@IF501-210-05>
2019-05-23 10:58:28 +0200

    Analyse wip

--- src/Analyse.cpp
+++ src/Analyse.cpp
@@ -246,6 +246,47 @@ void Analyse::CalculValeurAtmo(double O3, double NO2, double SO2, double PM10)
     
 }
 
+void Analyse::CapteursSimilaires(SensorFactory &sensorFactory)
+{
+	vector<string> polluants;
+	vector<Sensor> listeCapteurs = sensorFactory.GetSensors();
+
+	double **matriceCapteurs = new double*[listeCapteurs.size()];
+	for (int i = 0; i < listeCapteurs.size(); i++) {
+		matriceCapteurs[i] = new double[listeCapteurs.size()];
+	}
+
+	double similitudeMesure = 0;
+	int compteur = 0;
+	for (int i = 0; i < listeCapteurs.size(); i++)
+	{
+		for (int j = i + 1; j < listeCapteurs.size(); j++)
+		{
+			for (Mesure m : listeCapteurs[i].GetListeMesure())
+			{
+				if ((this->debut < m.GetDate()) && (m.GetDate() < this->fin))
+				{
+					for (Mesure n : listeCapteurs[j].GetListeMesure())
+					{
+						if (m < n && m.GetPolluant() == n.GetPolluant())
+						{
+							similitudeMesure += 1 - (m.GetValeur() - n.GetValeur() / m.GetValeur());
+							compteur++;
+							break;
+						}
+					}
+				}
+			}
+			matriceCapteurs[i][j] = similitudeMesure / compteur;
+			cout << matriceCapteurs[i] << endl;
+		}
+	}
+
+	for (int i = 0; i < listeCapteurs.size(); i++)
+		delete[] matriceCapteurs[i];
+	delete[] matriceCapteurs;
+}
+
 //------------------------------------------------- Surcharge d'opérateurs
 /*Analyse & Analyse::operator = ( const Analyse & unAnalyse )
 // Algorithme :
